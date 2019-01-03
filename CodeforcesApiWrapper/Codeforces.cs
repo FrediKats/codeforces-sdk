9704be93457bc5072a9ede5a9caea2519842cb3c
Fredi Kats <fredikats@outlook.com>
2019-01-03 04:17:56 +0300

    BlogEntity methods implementation with unit tests

---
+++ CodeforcesApiWrapper/Codeforces.cs
@@ -0,0 +1,15 @@
+﻿using CodeforcesApiWrapper.Methods;
+using Refit;
+
+namespace CodeforcesApiWrapper
+{
+    public class Codeforces
+    {
+        public Codeforces()
+        {
+            BlogEntity = RestService.For<IBlogEntityMethods>("http://codeforces.com/api");
+        }
+
+        public IBlogEntityMethods BlogEntity { get; }
+    }
+}
---
+++ CodeforcesApiWrapper/ResponseContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeforcesApiWrapper
+{
+    public class ResponseContainer <T>
+    {
+        public string Status { get; set; }
+        public string Comment { get; set; }
+        public T Result { get; set; }
+    }
+}
