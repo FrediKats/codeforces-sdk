6eb0d932f8e4926a6f039e2e801d82b6d567938e
Fredi Kats <fredikats@outlook.com>
2019-01-03 09:47:36 +0300

    Add user methods implemention

--- CodeforcesApiWrapper/Codeforces.cs
+++ CodeforcesApiWrapper/Codeforces.cs
@@ -13,11 +13,13 @@ namespace CodeforcesApiWrapper
             Contest = RestService.For<IContestMethods>(ApiUrl);
             ProblemSet = RestService.For<IProblemSetMethods>(ApiUrl);
             RecentActions = RestService.For<IRecentActionsMethods>(ApiUrl);
+            User = RestService.For<IUserMethods>(ApiUrl);
         }
 
         public IBlogEntityMethods BlogEntity { get; }
         public IContestMethods Contest { get; }
         public IProblemSetMethods ProblemSet { get; }
         public IRecentActionsMethods RecentActions { get; }
+        public IUserMethods User { get; }
     }
 }
---
+++ CodeforcesApiWrapper/Methods/IUserMethods.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeforcesApiWrapper.Types;
+using Refit;
+
+namespace CodeforcesApiWrapper.Methods
+{
+    public interface IUserMethods
+    {
+        [Get("/user.blogEntries")]
+        Task<ResponseContainer<List<BlogEntry>>> BlogEntries(string handle);
+
+        //TODO: Add user.friends
+
+        //TODO: Implement list as parameter
+        [Get("/user.info")]
+        Task<ResponseContainer<List<User>>> Info(string handles);
+
+        [Get("/user.ratedList")]
+        Task<ResponseContainer<List<User>>> RatedList(bool? activeOnly = null);
+
+        [Get("/user.rating")]
+        Task<ResponseContainer<List<RatingChange>>> Rating(string handle);
+
+        [Get("/user.status")]
+        Task<ResponseContainer<List<Submission>>> Status(string handle, int? from = null, int? count = null);
+    }
+}
